f74d9425253cbde6ed969d09a2521eb37e8a21e3
lyc <saber000@vip.qq.com>
2015-01-08 04:44:41 -0800

    update

---
+++ allowfail.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+from collections import namedtuple
+from functools import wraps
+
+
+AllowFailResult = namedtuple(
+    "AllowFailResult", ["ok", "result"])
+
+
+class AllowFail(object):
+    logger = logging.getLogger("OnError")
+
+    def __init__(self, label, *params, **options):
+        self.label = label
+        self.params = params
+        self.error_handler = options.pop("on_error", self.on_error)
+        self.logger = options.pop("logger", self.logger)
+
+    def on_error(self, label, err):
+        """On error handler
+        """
+        self.logger.warning("%s got an error: %s", label, err)
+
+    def __enter__(self):
+        """Use instance as a context manager to protect a block
+        """
+        return self
+
+    def __exit__(self, typ, val, trbk):
+        """Catch exceptions
+        """
+        if val:
+            self.error_handler(self.label % self.params, val)
+
+        # catch exceptions
+        return True
+
+    def __call__(self, func):
+        """Use instance as a decorator to protect a function call
+        """
+        @wraps(func)
+        def protect(*args, **kwg):
+            try:
+                result = AllowFailResult(ok=True, result=func(*args, **kwg))
+            except Exception as err:
+                result = AllowFailResult(ok=False, result=err)
+                with self:
+                    self.error_handler(self.label % self.params, err)
+            return result
+        return protect
+
+
+import unittest
+
+
+class TestUsage(unittest.TestCase):
+    def setUp(self):
+        self.cache = []
+
+    def log(self, *args, **kwg):
+        self.cache.append({
+            "args": args,
+            "kwg": kwg})
+
+    def test_decorator(self):
+        @AllowFail("test %s", "valueerror", on_error=self.log)
+        def valueerror():
+            raise ValueError("test exception")
+
+        @AllowFail("test %s", "func", on_error=self.log)
+        def func():
+            return 1
+
+        res = valueerror()
+        self.assertFalse(res.ok)
+        self.assertEqual(res.result.message, "test exception")
+        log = self.cache.pop()
+        self.assertEqual(log["args"][0], "test valueerror")
+        self.assertEqual(log["args"][1], res.result)
+
+        res = func()
+        self.assertTrue(res.ok)
+        self.assertEqual(res.result, 1)
+        self.assertListEqual(self.cache, [])
+
+    def test_contextmanager(self):
+        with AllowFail("test %s", "contextmanager", on_error=self.log):
+            raise ValueError()
+
+        log = self.cache.pop()
+        self.assertEqual(log["args"][0], "test contextmanager")
+        self.assertTrue(isinstance(log["args"][1], ValueError))
+
+
+if __name__ == "__main__":
+    unittest.main()
